bbc95e726fae714968f72f4879ea353280b5ea14
elfeny7 <ikmalfaris50@gmail.com>
2025-11-05 14:12:45 +0700

    fix(user): add loading on user for confirm modal

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -148,6 +148,7 @@ export default function User() {
                         setConfirmOpen(false);
                     }}
                     onCancel={() => setConfirmOpen(false)}
+                    loading={loading}
                 />
             )}
         </div>
